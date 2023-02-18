d5da697cb8e2245655820526e74d24a532844728
Mark Madi <markmadi232@gmail.com>
2023-02-18 22:37:26 +0200

    Created Nav Bar

---
+++ src/Components/FloatingNav.js
@@ -0,0 +1,72 @@
+import * as React from "react";
+
+export default function FloatingNav() {
+  return (
+    // <div className="gridContainer">
+    //   <div className="grid-item">
+    //     {" "}
+    //     <h2 className="h2nav item1">Fleet Status Dashbaord</h2>
+    //   </div>
+    //   <div className="grid-item">
+    //     {" "}
+    //     <p className="versionNav item2">
+    //       Version: <strong>Latest</strong>
+    //     </p>
+    //   </div>
+    //   <div className="grid-item item3">
+    //     {" "}
+    //     <button className="navButton">Edit</button>
+    //   </div>
+    // </div>
+    <div class="grid-container">
+      <div class="item1">
+        <h2 className="h2nav">Fleet Status Dashbaord</h2>
+      </div>
+      <div class="item2">
+        <p className="versionNav">
+          Version: <strong>Latest</strong>
+        </p>
+        <button className="navButton">Edit</button>
+
+      </div>
+      <div class="item3">3</div>
+      <div class="item4">4</div>
+      <div class="item5">5</div>
+      <div class="item6">6</div>
+      <div class="item7">7</div>
+      <div class="item8">8</div>
+      <div class="item9">9</div>
+      <div class="item10">10</div>
+      <div class="item11">11</div>
+      <div class="item12">12</div>
+      <div class="item13">13</div>
+      <div class="item14">14</div>
+      <div class="item15">15</div>
+      <div class="item16">16</div>
+      <div className="item17">17</div>
+      <div className="item18">18</div>
+      <div className="item19">19</div>
+      <div className="item20">20</div>
+      <div className="item21">21</div>
+      <div className="item22">22</div>
+      <div className="item23">23</div>
+      <div className="item24">24</div>
+      <div className="item25">25</div>
+      <div className="item26">26</div>
+      <div className="item27">27</div>
+      <div className="item28">28</div>
+      <div className="item29">29</div>
+      <div className="item30">30</div>
+      <div className="item31">31</div>
+      <div className="item32">32</div>
+      <div className="item33">33</div>
+      <div className="item34">34</div>
+      <div className="item35">35</div>
+      <div className="item36">36</div>
+      <div className="item37">37</div>
+      <div className="item38">38</div>
+      <div className="item39">39</div>
+      <div className="item40">40</div>
+    </div>
+  );
+}
